76de67ab0806ded1f4e0f5cb2e26c8f4b6def6b7
DK <driss.kabil@gmail.com>
2017-05-13 13:23:38 +0200

    S6L77 : Adding Navigation with Event Binding and ngIf - My version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  selectedView: string;
+
+  constructor(){
+    this.selectedView = "recipes";
+  }
+  
+  onViewSelected(selectedViewData: {selectedView: string}){
+    this.selectedView = selectedViewData.selectedView;
+  }
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  @Output('viewSelected') navClicked = new EventEmitter<{selectedView: string}>();
 
   constructor() { }
 
   ngOnInit() {
+  }
+  
+  onNavClick(selectedViewEvent: Event){
+    this.navClicked.emit({
+      selectedView: (<HTMLElement>selectedViewEvent.target).id
+    });
   }
 
 }
